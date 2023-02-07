871a12fe73cad04f9339ce63c208c5263a89b315
Afrikind <anthony.madukaku@acity.edu.gh>
2023-02-07 23:43:35 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import ResetPasswordPage from "./pages/authentication/reset-password-page";
 import ForgotPasswordPage from "./pages/authentication/forgot-password-page";
 import RegisterPage from "./pages/authentication/register-page";
 import RequireAuth from "./components/shared/require-auth";
-import AgrobookPage from "./pages/menus/AgroBros-page";
+import AgroBrosPage from "./pages/menus/AgroBros-page";
 import OurPlanPage from "./pages/plans/our-plan-page";
-import OurMealsPage from "./pages/menus/our-Services-page";
+import OurServicesPage from "./pages/menus/our-Services-page";
 
 function App() {
     const {themeVariant} = useSelector(selectUI);
@@ -26,11 +26,11 @@ function App() {
         <ThemeProvider theme={theme}>
             <Routes>
                 <Route element={<HomePage/>} exact={true} path="/"/>
-                <Route element={<CookbookPage/>} exact={true} path="/cookbook"/>
+                <Route element={<AgroBrosPage/>} exact={true} path="/Agrobros"/>
                 <Route element={<RequireAuth><ProfilePage/></RequireAuth>} exact={true} path="/profile"/>
                 <Route element={<RequireAuth><SettingsPage/></RequireAuth>} exact={true} path="/settings"/>
                 <Route element={<OurPlanPage/>} exact={true} path="/our-plan"/>
-                <Route element={<OurMealsPage/>} exact={true} path="/our-meals"/>
+                <Route element={<OurServicesPage/>} exact={true} path="/our-meals"/>
                 <Route element={<AboutPage/>} exact={true} path="/about-us"/>
                 <Route element={<RegisterPage/>} exact={true} path="/auth/register"/>
                 <Route element={<LoginPage/>} exact={true} path="/auth/login"/>
