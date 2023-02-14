dd7dd118a6e6fe5a777a15606558ea80f3f3844f
Afrikind <anthony.madukaku@acity.edu.gh>
2023-02-14 19:17:54 +0000

    Update notifications-page.js

--- src/pages/notifications/notifications-page.js
+++ src/pages/notifications/notifications-page.js
@@ -4,54 +4,11 @@ import {Container, Typography} from "@mui/material";
 const NotificationsPage = () => {
     return (
         <Layout>
-            Overlay
-            children={<Box sx={{height: '100%', display: 'flex', alignItems: 'center'}}>
             <Container>
-            <Grid container={true} spacing={4} alignItems="center">
-                        <Grid item={true} xs={12} md={6}>
-                <Typography variant="h2" align="center">
-                    The current weather is  31*c
-                </Typography>
                 <Typography variant="h4" align="center">
-                    Stay close to your internet to get live update on your soil water content
+                    Page under construction
                 </Typography>
-                <Button
-                sx={{
-                    textTransform: 'capitalize', width: {xs: '100%', md: '40%'},
-
-                }}
-                variant="contained"
-                disableElevation={true}
-                size="large"
-                color="secondary">
-                View soil
-            </Button>
-            <Button
-                sx={{
-                    textTransform: 'capitalize', width: {xs: '100%', md: '40%'},
-
-                }}
-                variant="contained"
-                disableElevation={true}
-                size="large"
-                color="secondary">
-                View reservoir content
-            </Button>
-
-            <Button
-                sx={{
-                    textTransform: 'capitalize', width: {xs: '100%', md: '40%'},
-                }}
-                variant="contained"
-                disableElevation={true}
-                size="large"
-                color="secondary">
-                sign-Out
-            </Button>
-            </Grid>
-            </Grid>
-                </Container>
-                </Box>}
+            </Container>
         </Layout>
     )
 }
