5ee5571bedce508e8e1d7e03739bb3eb78fba912
Afrikind <anthony.madukaku@acity.edu.gh>
2023-02-14 19:39:33 +0000

    yess

--- src/pages/plans/our-plan-page.js
+++ src/pages/plans/our-plan-page.js
@@ -5,8 +5,12 @@ const LikedTrailersPage = () => {
     return (
         <Layout>
             <Container>
-                <Typography variant="h4" align="center">
-                    Page under construction
+                <Typography variant="h2" align="center">
+                    we are going to connect you to your system => click the button below
+                </Typography>
+         <Typography variant="h2" align="center">
+                    click to check soil water level and get current weather Updates
+        Thanks and keep the Better dream to Africa
                 </Typography>
             </Container>
         </Layout>
