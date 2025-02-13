ee087378364ae35e45dab4dbb7106fed6ac98d0f
Nnaemeka Madukaku <afrikind99@gmail.com>
2025-02-12 18:30:22 -0800

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 const SERVER_BASE_URL = 'http://localhost:8000/api/v1';
-// const SERVER_BASE_URL = 'https://thebookstation.herokuapp.com/api/v1';
 const AgroBrothers_THEME_VARIANT = 'FOODBOX_THEME_VARIANT';
 const AgroBrothers_AUTH_TOKEN = 'FOODBOX_AUTH_TOKEN';
 const AgroBrothers_AUTH_DATA = 'FOODBOX_AUTH_DATA';
